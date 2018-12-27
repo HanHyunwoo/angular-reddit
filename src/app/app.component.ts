c0e4cd6917bc6cee21016b002e7ad8e6b04fe277
HAN <hhw1990@naver.com>
2018-12-27 11:29:58 +0900

    [추가] 텍스트,버튼 추가 및 내용 콘솔에 찍기

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+// production모드 변경 Angular is running in the development mode. Call enableProdMode() to enable the production mode. 
+import {enableProdMode} from '@angular/core';  
+enableProdMode();
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
 }
